eac18cf734fd5a6547d8f9b75cc1c2b3b9132ec1
lia-07 <107384739+lia-07@users.noreply.github.com>
2024-04-14 21:27:40 +1200

    add lf colours, remove flexoki

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,53 +6,28 @@ const config = {
 		extend: {
 			// Colours from Flexoki - https://stephango.com/flexoki
 			colors: {
-				base: {
-					black: '#100F0F',
-					950: '#1C1B1A',
-					900: '#282726',
-					850: '#343331',
-					800: '#403E3C',
-					700: '#575653',
-					600: '#6F6E69',
-					500: '#878580',
-					300: '#B7B5AC',
-					200: '#CECDC3',
-					150: '#DAD8CE',
-					100: '#E6E4D9',
-					50: '#F2F0E5',
-					paper: '#FFFCF0'
+				gray: {
+					1: '#f6f7fa',
+					2: '#D6D6D6',
+					3: '#A1A1A4',
+					4: '#818183',
+					5: '#6E6E6E',
+					6: '#222222'
 				},
-				red: {
-					DEFAULT: '#AF3029',
-					light: '#D14D41'
+				navy: {
+					royal: '#003778',
+					deep: '#002857',
+					dark: '#00183C'
 				},
-				orange: {
-					DEFAULT: '#BC5215',
-					light: '#DA702C'
-				},
-				yellow: {
-					DEFAULT: '#AD8301',
-					light: '#D0A215'
-				},
-				green: {
-					DEFAULT: '#66800B',
-					light: '#879A39'
-				},
-				cyan: {
-					DEFAULT: '#24837B',
-					light: '#3AA99F'
-				},
-				blue: {
-					DEFAULT: '#205EA6',
-					light: '#4385BE'
+				azure: {
+					base: '#0094FF',
+					deep: '#0077CC',
+					dark: '#00548F'
 				},
 				purple: {
-					DEFAULT: '#5E409D',
-					light: '#8B7EC8'
-				},
-				magenta: {
-					DEFAULT: '#A02F6F',
-					light: '#CE5D97'
+					royal: '#5B1DE7',
+					deep: '#3D14A3',
+					dark: '#11156D'
 				}
 			},
 			fontFamily: {
