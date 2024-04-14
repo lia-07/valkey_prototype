688e92997dd9011ae57f90d11573cbc35c54cfd5
lia-07 <107384739+lia-07@users.noreply.github.com>
2024-04-14 23:38:09 +1200

    improve responsiveness and add dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
-<div class="mx-auto my-8 max-w-screen-md overflow-clip rounded-sm">
+<div class="mx-auto max-w-screen-md overflow-clip rounded-sm md:my-8">
 	<header
-		class=" border-azure-base from-azure-dark via-navy-royal to-navy-royal text-gray-1 border-t-4 bg-gradient-to-b px-8 py-6"
+		class=" border-azure-base from-azure-dark via-navy-royal to-navy-royal text-gray-1 border-t-4 bg-gradient-to-b px-4 py-4 md:px-8 md:py-6"
 	>
 		<div class="flex items-center gap-2">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
-				class=" fill-gray-1 stroke-gray-1 mb-2 aspect-square h-12"
+				class=" fill-gray-1 stroke-gray-1 mb-2 aspect-square h-8 md:h-12"
 				viewBox="0 0 201.7 232.1"
 				><path
 					d="M60.2 23.7c-23 13.2-46 26.4-51 29.3l-6.7 3.8C1 57.6 0 59.3 0 61.1v106.1c0 1.7.9 3.3 2.3 4.2L20 182.5c11 6.9 20.4 12.8 20.8 13l.8.4-.1-15.2-.1-12.4c0-1.7-.9-3.3-2.3-4.2l-5.6-3.5-5.6-3.5c-1.5-.9-2.3-2.5-2.4-4.2l-.1-38.5.2-38.4c0-1.8 1-3.5 2.5-4.4l35.7-20.4 35.7-20.4c1.6-.9 3.5-.9 5 0L139 51.1l34.6 20.3c1.5.9 2.5 2.5 2.5 4.3v82.1c0 1.8-1 3.5-2.6 4.4l-34.9 19.6-34.8 19.6c-1.6.9-3.6.9-5.1-.1l-3.6-2.2-3.7-2.3c-1.5-.9-2.4-2.5-2.4-4.3V150c0-1.9-1.1-3.7-2.8-4.5-9.6-4.6-16.3-13.5-18.6-24.6-.8-3.8-.5-12.2.5-15.6C72 92.2 81.8 82.9 94.6 80.2c4.4-.9 10.1-.8 14.3.3 18.5 4.8 29.5 23.9 24.7 42.6-2.5 9.7-9 17.8-17.9 22.3-1.7.8-2.8 2.6-2.8 4.5v22.5l.8-.5c.4-.3 9.9-5.7 21.2-12l17.9-10.1c1.6-.9 2.5-2.6 2.5-4.4V87.7c0-1.8-.9-3.4-2.5-4.3l-24.2-14.3c-14.7-8.6-26.9-15.7-27.1-15.7-.2 0-12.7 7.1-27.8 15.7L48.9 83.4c-1.6.9-2.5 2.5-2.5 4.3v54.9c0 1.7.9 3.3 2.4 4.2l7.2 4.5 7.2 4.5c1.5.9 2.3 2.5 2.3 4.2v48c0 1.7.9 3.3 2.4 4.2l15.2 9.5 15 9.4c1.6 1 3.5 1 5.1.1l48-27 48-27.1c1.6-.9 2.5-2.6 2.5-4.4V61c0-1.8-.9-3.4-2.5-4.3l-47.3-27.8C128.3 15 108.4 3.3 103.3.3c-.8-.5-1.7-.5-2.5 0-4.5 2.6-21.1 12.1-40.6 23.4z"
@@ -14,7 +14,9 @@
 				/></svg
 			>
 
-			<h1 class="mb-2 font-archivo text-5xl font-bold">Valkey<sup class="">&reg;</sup></h1>
+			<h1 class="mb-2 font-archivo text-4xl font-bold md:text-5xl">
+				Valkey<sup class="">&reg;</sup>
+			</h1>
 		</div>
 		<nav class="flex flex-wrap gap-x-4 text-lg">
 			<a href="#" class="hover:underline">Home</a>
@@ -24,18 +26,20 @@
 			<a href="#" class="hover:underline">Source</a>
 		</nav>
 	</header>
-	<main class="border-gray-2 bg-gray-2/40 border shadow-sm">
-		<section class=" border-gray-1 border-b px-8 py-6">
-			<h1 class="text-gray-6 mb-2 max-w-xl font-archivo text-4xl font-medium">
+	<main class="border-gray-2 dark:border-gray-5/50 bg-gray-2/20 dark:bg-gray-5/20 border shadow-sm">
+		<section class=" border-gray-1 border-b px-4 py-4 md:px-8 md:py-6">
+			<h1
+				class="text-gray-6 dark:text-gray-1 dark:text-gray-1 mb-2 max-w-xl font-archivo text-3xl font-medium md:text-4xl"
+			>
 				Valkey is an astonishingly fast & open source data store.
 			</h1>
-			<h2 class="text-gray-5 mb-6 max-w-xl text-xl">
+			<h2 class="text-gray-5 dark:text-gray-2 mb-6 max-w-xl text-xl">
 				It is well suited for key/value workloads, yet also features a wide variety of native data
 				structures.
 			</h2>
 			<div class="flex gap-4">
 				<button
-					class="text-gray-1 border-navy-royal from-azure-deep via-navy-royal to-navy-royal flex items-center gap-2 rounded border bg-gradient-to-b px-3 py-2 font-archivo font-bold shadow-inner drop-shadow transition-all hover:scale-[101%] hover:brightness-105 hover:drop-shadow-md active:scale-95 active:brightness-95"
+					class="text-gray-1 border-navy-royal dark:border-azure-base from-azure-deep via-navy-royal to-navy-royal flex items-center gap-2 rounded border bg-gradient-to-b px-3 py-2 font-archivo font-bold shadow-inner drop-shadow transition-all hover:scale-[101%] hover:brightness-105 hover:drop-shadow-md active:scale-95 active:brightness-95"
 					>Downloads
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -52,57 +56,83 @@
 				</button>
 
 				<button
-					class=" border-gray-2 bg-gray-1 text-gray-6 flex items-center gap-2 rounded border px-3 py-2 font-archivo font-medium drop-shadow-sm transition-all hover:scale-[101%] hover:drop-shadow active:scale-95 active:brightness-95"
+					class=" border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 text-gray-6 dark:text-gray-1 flex items-center gap-2 rounded border px-3 py-2 font-archivo font-medium drop-shadow-sm transition-all hover:scale-[101%] hover:drop-shadow active:scale-95 active:brightness-95"
 					>Documentation
 				</button>
 			</div>
 		</section>
 
-		<section class="border-t-gray-2 border-t px-8 py-6">
-			<h1 class="text-navy-royal mb-2 font-archivo text-3xl font-bold">Key features</h1>
-			<div class=" grid grid-cols-3 gap-2">
+		<section class="border-t-gray-2 border-t px-4 py-4 md:px-8 md:py-6">
+			<h1 class="text-navy-royal dark:text-gray-2 mb-2 font-archivo text-3xl font-bold">
+				Key features
+			</h1>
+			<div class=" grid grid-cols-1 gap-2 md:grid-cols-3">
 				<div class="grid-span-1 flex flex-col gap-2">
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Super speedy</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Super speedy
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Valkey is an <i>in memory</i> data store, making it faster than most disk based solutions.
 						</p>
 					</article>
 
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Many data structures</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Many data structures
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Valkey could be described as a "data structure server". It supports strings, hashes,
 							lists, sets, streams, & many more.
 						</p>
 					</article>
 				</div>
 				<div class="grid-span-1 flex flex-col gap-2">
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Persistent</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Persistent
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Valkey saves all writes to permanent storage to survive reboots and system failures.
 						</p>
 					</article>
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Extensible</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Extensible
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Valkey has a modular API, facilitating the creation of custom extensions in C, C++, or
 							Rust.
 						</p>
 					</article>
 				</div>
 				<div class="grid-span-1 flex flex-col gap-2">
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Scalable</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Scalable
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Valkey supports horizontal scalability with hash-based sharding. It can automatically
 							re-partition when growing the cluster.
 						</p>
 					</article>
-					<article class="border-gray-2 bg-gray-1 col-span-1 rounded border px-3 py-2">
-						<h1 class="text-archivo text-gray-6 mb-1 text-xl font-bold">Highly Avaliable</h1>
-						<p class="text-gray-6">
+					<article
+						class="border-gray-2 dark:border-gray-5 bg-gray-1 dark:bg-gray-6 col-span-1 rounded border px-3 py-2"
+					>
+						<h1 class="text-archivo text-gray-6 dark:text-gray-1 mb-1 text-xl font-bold">
+							Highly Avaliable
+						</h1>
+						<p class="text-gray-6 dark:text-gray-1">
 							Replication with automatic failover for both standalone and clustered deployments.
 						</p>
 					</article>
